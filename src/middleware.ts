1731e3c40dfde97b73911c021843248480003e35
Mike Goldenberg <csrutil@tuta.io>
2023-10-19 14:59:18 +0200

    utils, lib for the project

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,26 @@
 // Resource: https://clerk.com/docs/nextjs/middleware#auth-middleware
 // Copy the middleware code as it is from the above resource
 
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
+import { NextRequest } from 'next/server';
 
 export default authMiddleware({
   // An array of public routes that don't require authentication.
-  publicRoutes: ["/api/webhook/clerk"],
+  publicRoutes: (req: NextRequest) =>
+      !req.nextUrl.pathname.startsWith('/dashboard'),
+
+  // eslint-disable-next-line consistent-return
+  afterAuth(auth, req) {
+    // handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+  },
 
   // An array of routes to be ignored by the authentication middleware.
-  ignoredRoutes: ["/api/webhook/clerk"],
+  ignoredRoutes: ['/api/webhook/clerk'],
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)'],
 };
---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+// FIXME: Update this configuration file based on your project information
+
+export const AppConfig = {
+  site_name: 'Starter for social network app',
+  title: 'Nextjs starterdeez',
+  description: 'Starter code for the social network app with Nextjs and Tailwind CSS',
+  locale: 'env',
+};
