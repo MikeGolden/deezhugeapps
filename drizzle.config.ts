799a161a99774c2d7c4c8391719c0a65af99b50d
Mike Goldenberg <csrutil@tuta.io>
2023-10-19 13:57:46 +0200

    configs and scripts

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { type } from 'drizzle-kit';
+
+/** @type {import('drizzle-kit').Config} */
+export default {
+  out: './migrations',
+  schema: './src/models/schema.ts',
+  driver: 'libsql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL ?? '',
+  },
+} satisfies Config;
