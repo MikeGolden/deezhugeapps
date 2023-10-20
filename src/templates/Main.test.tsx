9ec42a18ff6c4c7df6f4fdfbf19425e55419f331
Mike Goldenberg <csrutil@tuta.io>
2023-10-20 10:29:55 +0200

    main ui template and test

---
+++ src/templates/Main.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen, within } from '@testing-library/react';
+import { Main } from './Main';
+
+describe('Main template', () => {
+  describe('Render method', () => {
+    it('should have 8 menu items', () => {
+      render(<Main meta={null}>{null}</Main>);
+
+      const menuItemList = screen.getAllByRole('listitem');
+
+      expect(menuItemList).toHaveLength(8);
+    });
+
+    it('should have a link to github', () => {
+      render(<Main meta={null}>{null}</Main>);
+
+      const copyrightSection = screen.getByText(/Copyright/);
+      const copyrightLink = within(copyrightSection).getByRole('link');
+
+      expect(copyrightLink).toHaveAttribute(
+        'href',
+        'https://github.com/MikeGolden',
+      );
+    });
+  });
+});
